95c38b4e351ea1ecac415dbea1c4cf13f68a286d
Tom Delaney <tom.delaney@8451.com>
2018-02-09 16:23:18 -0500

    Pushing build artifact from Github (letting Heroku rebuild and not deploy a docker container)

--- src/main/java/com/example/devopsservice/DevOpsServiceApplication.java
+++ src/main/java/com/example/devopsservice/DevOpsServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevOpsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevOpsServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DevOpsServiceApplication.class, args);
+  }
 }
