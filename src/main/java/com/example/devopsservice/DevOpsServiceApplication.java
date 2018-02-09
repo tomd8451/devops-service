9d2c796eb6232f70e3df01645811e81ff618cdad
Tom Delaney <tom.delaney@8451.com>
2018-02-09 14:52:05 -0500

    Fixing a spelling typo in DevOps

--- src/main/java/com/example/devopsservice/DevOpsServiceApplication.java
+++ src/main/java/com/example/devopsservice/DevOpsServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsServiceApplication {
+public class DevOpsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsServiceApplication.class, args);
+		SpringApplication.run(DevOpsServiceApplication.class, args);
 	}
 }
